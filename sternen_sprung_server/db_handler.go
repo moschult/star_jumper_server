c49af84b2119ed60e5f24b9bb361caf3f5cd6e4b
Moritz Schult <moschult@uni-bremen.de>
2018-03-08 21:58:13 +0100

    adds db insertions and deletions

--- sternen_sprung_server/db_handler.go
+++ sternen_sprung_server/db_handler.go
@@ -10,10 +10,14 @@ var (
 )
 
 func main() {
-	initiallyCreate();
-	create_star_systems();
-	create_planets();
-	create_belts();
+	initiallyCreate()
+	create_star_systems()
+	create_planets()
+	create_belts()
+	populate()
+	getAllBelts()
+	dropTable("belts", 5)
+	getAllBelts()
 }
 
 func initiallyCreate() (*sql.DB) {
@@ -97,6 +101,78 @@ func create_belts() (){
 
 }
 
-func populate()  {
+func populate() {
+	db, err := sql.Open("mysql", "moritz:moritz@tcp(127.0.0.1:3306)/")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close();
+	_, err = db.Exec("USE " + db_name)
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	_, err = db.Exec("INSERT INTO belts VALUES (5, 3)")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added Data to Table Belts")
+	}
+}
+
+	func getAllBelts() {
+		fmt.Println("Printing belts")
+		var (
+			id int
+			ressources string
+		)
+		db, err := sql.Open("mysql", "moritz:moritz@tcp(127.0.0.1:3306)/")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer db.Close();
+		_,err = db.Exec("USE "+db_name)
+		if err != nil {
+			fmt.Println(err)
+		}
+		rows, err := db.Query("select * from belts")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer rows.Close()
+
+		fmt.Println("Closed rows")
+		for rows.Next() {
+			err := rows.Scan(&id, &ressources)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(id, ressources)
+		}
+		err = rows.Err()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	/**
+	Should only be used for debugging, handle with care
+	 */
+func dropTable(tableName string, id int) () {
+	fmt.Printf("Dropping table %s\n", tableName)
+	db, err := sql.Open("mysql", "moritz:moritz@tcp(127.0.0.1:3306)/")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close();
+	_, err = db.Exec("USE " + db_name)
+	if err != nil {
+		fmt.Println(err)
+	}
+	_, err = db.Exec("DElETE FROM " + tableName + " WHERE id=?", id)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Dropped Table %s\n", tableName)
+	}
 }
